d3ab02e30b50d1110ac6c6166ddcdcb87cfd8b75
Shady <shady.achouri@gmail.com>
2019-12-27 15:33:42 +0100

    Updates

---
+++ precache-manifest.0ab0ad7c612482a34d642406317a1ac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef47e56d412adc70fb693e4e6c07fc43",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5146b52be3db6fa6e2d3",
+    "url": "/monsters-rolodex/static/css/main.649a3980.chunk.css"
+  },
+  {
+    "revision": "72e57426f2a50eae9509",
+    "url": "/monsters-rolodex/static/js/2.c710127d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.c710127d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5146b52be3db6fa6e2d3",
+    "url": "/monsters-rolodex/static/js/main.ed2dd108.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
